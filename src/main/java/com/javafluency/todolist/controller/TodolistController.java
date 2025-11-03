475cfa0dbe8b1902b86a85416204b3c405152fc6
Angélica Martins <marttinsangelica@gmail.com>
2025-11-03 15:01:22 -0300

    feat: add ResponseStatus in delete

--- src/main/java/com/javafluency/todolist/controller/TodolistController.java
+++ src/main/java/com/javafluency/todolist/controller/TodolistController.java
@@ -6,6 +6,7 @@ import com.javafluency.todolist.service.TodolistService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -56,11 +58,10 @@ public class TodolistController {
   }
 
   @DeleteMapping("/{todoId}")
-  public ResponseEntity<Void> deleteTodo(@PathVariable Long todoId) {
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  public void deleteTodo(@PathVariable Long todoId) {
     log.info("Deleting todo. todoId: {}", todoId);
 
     todolistService.deleteTodo(todoId);
-
-    return ResponseEntity.noContent().build();
   }
 }
